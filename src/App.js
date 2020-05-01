da3632ce062fa6ea0592688bf87370c67bcc9291
nildemarco <nildemarco@gmail.com>
2020-04-30 22:23:02 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import arboles from './assets/arboles.jpg';
+import styled from 'styled-components'
+import InputContainer from './components/InputContainer'
 import './App.css';
 
-function App() {
+const MainContainer = styled.main`
+width: 100%;
+padding: 10px;
+color: #ffff;
+font-family: Arial, Helvetica, sans-serif;
+font-size: 38px;
+font-weight: 100;
+background-color: #193549;
+display: flex;
+flex-direction: column;
+align-items: center;
+h1 {
+  margin: 10px;
+}
+span{
+  color: #ffc600;
+}
+`
+const ContainerImg = styled.div`
+width: 100%;
+height: auto;
+display: flex;
+justify-content: center;
+`
+const ImageToChange = styled.img`
+  border-radius: ${({ radio }) => `${radio}%`};
+  padding: ${({ spacing }) => `${spacing}px`};
+  background-color:${ ({ color }) => `${color}`};
+  filter: ${({grayscale})=> `grayscale(${grayscale}%)`}; 
+ `
+
+const App = () => {
+  const [style, setstyle] = useState({
+    grayscale: 0,
+    spacing: 5,
+    radio: 0,
+    color: "black",
+  })
+
+  const handleChange = e => {
+    setstyle({ ...style, [e.target.name]: e.target.value })
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MainContainer>
+      <h1>Update Style with <span>React</span></h1>
+       <InputContainer 
+       style={style}
+       handleChange={handleChange}/>
+      <ContainerImg>
+      <ImageToChange 
+      src={arboles} 
+      radio={style.radio} 
+      spacing={style.spacing} 
+      grayscale={style.grayscale} 
+      color={style.color}/>
+      </ContainerImg>
+    </MainContainer>
   );
 }
 
---
+++ src/components/InputContainer.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+width: 100%;
+display: flex;
+justify-content: space-around;
+align-items: center;
+color: #ffff;
+margin: 38px;
+label {
+  font-weight: 100;
+  font-size: 30px;
+}
+input {
+  text-align: center;
+  padding: 5px;
+  margin: 0px;
+
+}`
+
+const InputContainer = ({ style, handleChange }) => {
+  return (
+    <Container>
+      <label>Radius:</label>
+      <input type='range'
+        name='radio'
+        max='50'
+        min='0'
+        defaultValue={0}
+        value={style.radio}
+        onChange={handleChange}></input>
+
+      <label> Spacing:</label>
+      <input type='range'
+        name='spacing'
+        max='100'
+        min='0'
+        defaultValue={0}
+        value={style.spacing}
+        onChange={handleChange}>
+      </input>
+
+      <label> GrayScale:</label>
+      <input type='range'
+        name='grayscale'
+        max='100'
+        min='0'
+        defaultValue={0}
+        value={style.grayscale}
+        onChange={handleChange}>
+      </input>
+
+      <label> Color:</label>
+      <input type="color"
+        name='color'
+        value={style.color}
+        onChange={handleChange}>
+      </input>
+
+    </Container>
+  );
+}
+export default InputContainer;
